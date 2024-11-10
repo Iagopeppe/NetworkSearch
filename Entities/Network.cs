9a0ed33050f2f18ea49f9b08d4b2355a639dc1ed
Iago Tambosi <60783538+Iagopeppe@users.noreply.github.com>
2024-11-10 16:38:01 -0300

    Add project files.

---
+++ Entities/Network.cs
@@ -0,0 +1,96 @@
+﻿namespace NetworkSearch.Entities
+{
+    internal class Network
+    {
+        private List<int> Elements { get; set; }
+
+        private Dictionary<int, List<int>> Connections { get; set; }
+
+        public Network(int qntElementos)
+        {
+            Elements = new List<int>();
+            Connections = new Dictionary<int, List<int>>();
+            Random random = new Random();
+
+            try
+            {
+                if (qntElementos < 1)
+                    throw new Exception("Quantidade de elementos deve ser maior que 0");
+
+                for (int i = 1; i <= qntElementos; i++)
+                {
+                    Elements.Add(i);
+                    Connections.Add(i, new List<int>());
+                    Console.WriteLine("Elemento " + i + " criado");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro ao criar Network: " + e.Message);
+            }
+        }
+
+        public void connect(int source, int destination)
+        {
+            if (!Elements.Contains(source))
+            {
+                Console.WriteLine($"Elemento de origem não encontrado: {source}");
+                return;
+            }
+            else if (!Elements.Contains(destination))
+            {
+                Console.WriteLine($"Elemento de destino não encontrado: {destination}");
+                return;
+            }
+            else if (Connections[source].Contains(destination))
+            {
+                Console.WriteLine($"Conexão entre {source} e {destination} já existe");
+                return;
+            }
+
+            Connections[source].Add(destination);
+            Connections[destination].Add(source);
+
+            Console.WriteLine($"Conexão entre {source} e {destination} criada");
+        }
+
+        public bool query(int source, int destination)
+        {
+            if (!Elements.Contains(source))
+            {
+                Console.WriteLine($"Elemento de origem não encontrado: {source}");
+                return false;
+            }
+            else if (!Elements.Contains(destination))
+            {
+                Console.WriteLine($"Elemento de destino não encontrado: {destination}");
+                return false;
+            }
+
+            HashSet<int> searchNodes = new HashSet<int> { source };
+            Queue<int> queue = new Queue<int>(searchNodes);
+
+            while (queue.Count > 0)
+            {
+                int node = queue.Dequeue();
+
+                if (node == destination)
+                {
+                    Console.WriteLine($"Existe conexão entre {source} e {destination}");
+                    return true;
+                }
+
+                foreach (var connectedNode in Connections[node])
+                {
+                    if (searchNodes.Add(connectedNode))
+                    {
+                        queue.Enqueue(connectedNode);
+                    }
+                }
+            }
+
+            Console.WriteLine($"Não existe conexão entre {source} e {destination}");
+            return false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using NetworkSearch.Entities;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Insira o número de elementos do objeto Network (Exemplo: 7):");
+        int numberOfElements = int.Parse(Console.ReadLine());
+
+        var network = new Network(numberOfElements);
+
+        while (true)
+        {
+            Console.WriteLine("Escolha uma das seguintes opções: 1) Conectar elementos 2) Verificar conexão 3) Sair");
+            int option = int.Parse(Console.ReadLine());
+
+            if (option == 1)
+            {
+                Console.WriteLine("Insira o elemento de origem da conexão:");
+                int element1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Insira o elemento de destino da conexão:");
+                int element2 = int.Parse(Console.ReadLine());
+                network.connect(element1, element2);
+            }
+            else if (option == 2)
+            {
+                Console.WriteLine("Insira o elemento de origem:");
+                int element1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Insira o elemento de destino:");
+                int element2 = int.Parse(Console.ReadLine());
+                bool isConnected = network.query(element1, element2);
+            }
+            else if (option == 3)
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Invalid option. Please try again.");
+            }
+        }
+    }
+}
